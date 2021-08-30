b07b123115dec1790d171377a024c2f693ba3148
张亚飞 <672233580@qq.com>
2021-08-30 13:51:03 +0800

    Initial Commit

---
+++ AnimatedIndecator/AnimatedIndecatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIndecatorApp.swift
+//  AnimatedIndecator
+//
+//  Created by 张亚飞 on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIndecatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
